28b0d8dc8a51c4cd00ddfae1abd13c46c819e6a8
Joey Bodoia <jbodoia21@cmc.edu>
2020-12-18 14:29:39 -0800

    log out button and functionality added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,23 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import {useAppState} from "../AppState.jsx"
 
 const Nav = (props) => {
+
+    const { state, dispatch } = useAppState()
+
+
     return(
         <header>
             <h1>App title</h1>
             <nav>
                 <Link to="/"><div>Home</div></Link>
-                <Link to="/auth/signup"><div>Sign up</div></Link>
-                <Link to="/auth/login"><div>Log in</div></Link>
+                {!state.token ? (<><Link to="/auth/signup"><div>Sign up</div></Link>
+                <Link to="/auth/login"><div>Log in</div></Link></>) : null}
+                {state.token ? <div onClick={() => {
+                    dispatch({type: "logout"})
+                    props.history.push("/")
+                }}>Logout</div> : null}
 
             </nav>
         </header>
